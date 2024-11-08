f4c38ea068bcc71ce37b6dd5724f0ef8093ceded
guiqsassi <guiqsassi@gmail.com>
2024-11-08 11:23:15 -0300

    feat: criacao de repositorio de evento

---
+++ server/backend/Hackon/src/main/java/com/hackon/repositories/EventRepository.java
@@ -0,0 +1,27 @@
+package com.hackon.repositories;
+
+import com.hackon.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e"
+    + " WHERE" +
+            " e.location = :location" +
+            " AND (e.startDateTime BETWEEN :start and :end" +
+            " OR e.endDateTime BETWEEN :start and :end" +
+            " OR :start BETWEEN e.startDateTime and e.endDateTime"+
+            " OR :end BETWEEN e.startDateTime and e.endDateTime)"
+    )
+
+    List<Event> findEventsBetween(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("location") String location);
+
+}
