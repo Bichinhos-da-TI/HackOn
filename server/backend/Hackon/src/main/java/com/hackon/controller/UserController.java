58a2634bc4fe315b8f330af6242ee3f1495e8f0d
Matheus Dorigon <dorigon.matheus8@gmail.com>
2024-11-20 18:57:21 -0300

    testei e aparentemente esta funcionando

--- server/backend/Hackon/src/main/java/com/hackon/controller/UserController.java
+++ server/backend/Hackon/src/main/java/com/hackon/controller/UserController.java
@@ -5,6 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hackon.services.UserService;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.hackon.dto.UserDto;
+import com.hackon.dto.request.CreateUserDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -14,4 +26,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping()
+    public ResponseEntity<UserDto> create(@RequestBody CreateUserDto createUserDto) throws Exception {  
+        System.out.println(createUserDto);      
+        return userService.create(createUserDto);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findUser(@PathVariable Long id) throws Exception {
+        return userService.findUserById(id);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> allUsers() {
+        return userService.listUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) throws Exception {
+        return userService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> edit(@PathVariable Long id, @RequestBody UserDto userDto) throws Exception {
+        return userService.update(id,userDto);
+    }
+    
+    
 }
---
+++ server/backend/Hackon/src/main/java/com/hackon/dto/mappers/UserMapper.java
@@ -0,0 +1,45 @@
+package com.hackon.dto.mappers;
+
+import org.springframework.stereotype.Component;
+
+import com.hackon.dto.UserDto;
+import com.hackon.dto.enums.UserRoleEnum;
+import com.hackon.dto.request.CreateUserDto;
+import com.hackon.entities.User;
+
+@Component
+public class UserMapper {
+    public UserDto toDto(User user){
+        if(user == null){
+            return null;
+        }
+        return new UserDto(user.getId(), user.getName(), user.getUsername(), user.getPassword(), toEnum(user.getRole()));
+    }
+
+    
+    public User toEntity(UserDto userDto)throws Exception{
+        if(userDto == null){
+            return null;
+        }
+        return new User(userDto.id(), userDto.name(), userDto.username(), userDto.password(), toEntityEnum(userDto.role()));
+    }
+
+    public User toEntity(CreateUserDto createUserDto)throws Exception{
+        if(createUserDto == null){
+            return null;
+        }
+        return new User(createUserDto.name(), createUserDto.username(), createUserDto.password(), toEntityEnum(createUserDto.role()));
+    }
+
+    public UserRoleEnum toEnum(String role) {
+        if(role.equals(UserRoleEnum.ADMIN.toString()))return UserRoleEnum.ADMIN;
+        if(role.equals(UserRoleEnum.USER.toString()))return UserRoleEnum.USER;
+        throw new IllegalArgumentException("Role inválido: " + role);
+    }
+
+    public String toEntityEnum(UserRoleEnum role){
+        if(role.equals(UserRoleEnum.ADMIN))return UserRoleEnum.ADMIN.toString();
+        if(role.equals(UserRoleEnum.USER))return UserRoleEnum.USER.toString();
+        throw new IllegalArgumentException("Role inválido: " + role);
+    }
+}
